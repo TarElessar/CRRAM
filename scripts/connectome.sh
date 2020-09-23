1ad3cbea03849f97de3c5136e74dead671115932
TarElessar <felix88keys@aol.com>
2020-09-23 10:51:41 -0700

    Delete connectome.sh

--- scripts/connectome.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#/imaging/mc04/fm03/scripts/cbufsl_branch -l 1 -x "python /imaging/mc04/fm03/scripts/connectome.py $1"
-source /imaging/local/software/freesurfer/latest/x86_64/SetUpFreeSurfer.sh
-python /imaging/mc04/fm03/scripts/connectome.py $1
-
